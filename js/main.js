d64df3a3d0c04f0a9a067c999e83534ee3b7287e
elbmo <302090742@student.rocmondriaan.nl>
2024-10-16 10:06:39 +0200

    alles

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+const inputNickname = document.querySelector('input.input-nickname');
+const inputLeeftijd = document.querySelector('input.input-leeftijd');
+const buttonSend = document.querySelector('.button-send');
+const buttonStuur = document.querySelector('.button-stuur');
+const resultPara = document.querySelector('.result-para');
+
+buttonSend.addEventListener('click', function() {
+    let nickname = inputNickname.value;
+
+    if (nickname === "") {
+        alert("Je moet wel een nickname invoeren!");
+    } else if (nickname.length <= 2) {
+        alert("Het moet groter dan 2 tekens!");
+    } else {
+        console.log("Hallo " + nickname);
+        resultPara.textContent = `Welkom ${nickname} op de pagina`;
+    }
+});
+
+buttonStuur.addEventListener('click', function() {
+    let leeftijd = Number(inputLeeftijd.value);
+
+    if (leeftijd < 18) {
+        alert("Jij bent geen 18");
+    } else if (leeftijd === 18) {
+        alert("Jij bent 18");
+    } else if (leeftijd > 18) {
+        alert("Jij bent ouder dan 18");
+    }
+});
